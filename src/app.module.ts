ec3b3d433a4e0c4e6b30a16073c4bc50ee63c23a
yee2542 <yee2542gtl@gmail.com>
2021-02-03 02:23:53 +0700

    fix gql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
+import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
@@ -16,6 +17,6 @@ import { DatabaseModule } from './database/database.module';
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
