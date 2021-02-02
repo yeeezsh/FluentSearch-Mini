f2e491277b44f2ea39ede3a7bfff848e9c8bd478
yee2542 <yee2542gtl@gmail.com>
2021-02-03 03:11:26 +0700

    change field name

--- src/file/@dtos/file.create.dto.ts
+++ src/file/@dtos/file.create.dto.ts
@@ -2,7 +2,8 @@ import { ApiProperty } from '@nestjs/swagger';
 import { FileInfoDto } from './file.info.dto';
 export class CreateFileDto {
   @ApiProperty()
-  message: string;
+  owner: string;
+
   @ApiProperty({ type: FileInfoDto })
   file: FileInfoDto;
 }
