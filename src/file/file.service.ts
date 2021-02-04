a007de9ff8dbead9ec0d8bf1f2384166344c7bb4
yee2542 <yee2542gtl@gmail.com>
2021-02-04 23:38:40 +0700

    add get file service

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,18 +1,60 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { FileTypeEnum, ZoneEnum } from 'fluentsearch-types';
 import { Model } from 'mongoose';
+import filePathJoin from 'src/common/utils/file-path-join';
+import { APP_CONFIG } from 'src/config/config.constant';
+import { ConfigurationInterface } from 'src/config/config.interface';
 import { CreateFileDto } from './@dtos/file.create.dto';
 import { HTTPFile } from './@interfaces/http-file.interface';
 import { FileStoreService } from './file-store.service';
 import { FILE_MODEL } from './file.providers';
+import { ImageFileWithInsight } from './models/image-file-w-insight.model';
 import { AllFile, AllFileDoc } from './schema/file.schema';
 
 @Injectable()
 export class FileService {
   constructor(
     private filesService: FileStoreService,
+    @Inject(APP_CONFIG) private readonly appConfig: ConfigurationInterface,
     @Inject(FILE_MODEL) private readonly fileModel: Model<AllFileDoc>,
   ) {}
+
+  async getFilesWithInsight(
+    userId: string,
+    limit = 100,
+    skip = 0,
+  ): Promise<ImageFileWithInsight[]> {
+    const files = await this.fileModel
+      .find({ owner: userId })
+      .skip(skip)
+      .limit(limit);
+    console.log('files', files);
+    const mapped: ImageFileWithInsight[] = files.map((file: AllFile) => {
+      return {
+        _id: file._id,
+        label: file.label,
+        owner: file.owner,
+        meta: {
+          size: file.meta.size,
+          filename: file.label,
+          extension: file.meta.extension,
+          contentType: file.meta.contentType,
+          width: file.meta.width,
+          height: file.meta.height,
+          dpi: 72,
+        },
+        zone: file.zone,
+        type: FileTypeEnum.Image,
+        insight: [],
+
+        createAt: file.createAt,
+        updateAt: file.updateAt,
+        uri: filePathJoin(this.appConfig.hostname, file._id),
+      };
+    });
+
+    return mapped;
+  }
 
   async createFiles(files: HTTPFile[], body: CreateFileDto): Promise<void> {
     for (const file of files) {
@@ -37,6 +79,7 @@ export class FileService {
         createAt: new Date(),
         updateAt: new Date(),
       };
+
       await this.fileModel.create(parse);
     }
   }
