1b9655d15e345c0b691748aefa10bbeae2111c38
yee2542 <yee2542gtl@gmail.com>
2021-02-05 02:32:24 +0700

    import config module

--- src/insight/insight.module.ts
+++ src/insight/insight.module.ts
@@ -1,4 +1,5 @@
 import { HttpModule, Module } from '@nestjs/common';
+import { ConfigModule } from 'src/config/config.module';
 import { FileModule } from 'src/file/file.module';
 import { HttpConfigService } from './http.config.service';
 import { InsightWorkerService } from './insight-worker.service';
@@ -6,6 +7,7 @@ import { InsightService } from './insight.service';
 
 @Module({
   imports: [
+    ConfigModule,
     FileModule,
     HttpModule.registerAsync({ useClass: HttpConfigService }),
   ],
