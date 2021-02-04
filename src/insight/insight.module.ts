6642bee7ee1fd4540f5ee3d70e3ef755f5719137
yee2542 <yee2542gtl@gmail.com>
2021-02-04 20:10:26 +0700

    add on init

--- src/insight/insight.module.ts
+++ src/insight/insight.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, Module } from '@nestjs/common';
 import { FileModule } from 'src/file/file.module';
+import { HttpConfigService } from './http.config.service';
+import { InsightWorkerService } from './insight-worker.service';
 import { InsightController } from './insight.controller';
 import { InsightService } from './insight.service';
 
 @Module({
-  imports: [FileModule],
-  providers: [InsightService],
+  imports: [
+    FileModule,
+    HttpModule.registerAsync({ useClass: HttpConfigService }),
+  ],
+  providers: [InsightService, InsightWorkerService],
   controllers: [InsightController],
 })
 export class InsightModule {}
