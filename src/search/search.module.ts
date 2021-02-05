34ace73c919c361d91182d42ff8f0dfedbd95df8
yee2542 <yee2542gtl@gmail.com>
2021-02-06 02:13:34 +0700

    import config module

--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from 'src/config/config.module';
+import { FileModule } from 'src/file/file.module';
+import { InsightModule } from 'src/insight/insight.module';
 import { SearchService } from './search.service';
 
 @Module({
-  providers: [SearchService]
+  imports: [ConfigModule, FileModule, InsightModule],
+  providers: [SearchService],
 })
 export class SearchModule {}
