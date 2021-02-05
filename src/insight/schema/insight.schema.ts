739bb39190344ff724844373a0794d9f5477cd0a
yee2542 <yee2542gtl@gmail.com>
2021-02-05 09:06:58 +0700

    export insight doc

--- src/insight/schema/insight.schema.ts
+++ src/insight/schema/insight.schema.ts
@@ -1,7 +1,7 @@
 import { InsightSchema } from 'fluentsearch-types';
 import { Document, Schema } from 'mongoose';
 
-type InsightDoc = Document<InsightSchema>;
+export type InsightDoc = Document<InsightSchema>;
 
 export const insightSchema = new Schema<InsightDoc>({
   result: { type: String, required: true },
